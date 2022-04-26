e5fed147cf2a111b73653c893fdfb703d7066a81
Sidy Guedes <sidy_guedes@econeteditora.com.br>
2022-04-26 13:37:24 -0300

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+      <v-container>
+        <Nuxt />
+      </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div align="center" class="main container">
+    <v-container class="mt-10">
+      <v-avatar  color="grey lighten-2" size="128">
+        <v-img class="profile-picture" src="profile.jpeg" alt="SG"></v-img>
+      </v-avatar>
+      <p class="name-title title pt-5 font-weight-bold">Sidy Guedes</p>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style lang="scss">
+  .main {
+    .profile-picture {
+      border: 4px solid #57baed;
+    }
+    .name-title {
+
+    }
+
+  }
+</style>
