3b4d348e8fd06c20d2dddceed187c2575c3f24d0
Sidy Guedes <sidgley_guedes@hotmail.com>
2022-04-30 00:17:10 -0300

    new features

--- pages/index.vue
+++ pages/index.vue
@@ -7,35 +7,33 @@
       <div class="mt-3 name">
         <span class="title pt-5 font-weight-bold">Sidy Guedes</span>
       </div>
-      <div class="">
+      <div>
         <span class="caption pt-5 font-weight-bold">Web developer</span>
       </div>
       <div class="social-media text-center pa-5">
         <a class="px-1" href="#" target="_blank">
-          <v-icon color="#323232">mdi-instagram</v-icon>
+          <v-icon class="media-icon" color="#323232">mdi-instagram</v-icon>
         </a>
         <a class="px-1" href="#" target="_blank">
-          <v-icon color="#323232">mdi-linkedin</v-icon>
+          <v-icon class="media-icon" color="#323232">mdi-linkedin</v-icon>
         </a>
         <a class="px-1" href="#" target="_blank">
-          <v-icon color="#323232">mdi-github</v-icon>
+          <v-icon class="media-icon" color="#323232">mdi-github</v-icon>
         </a>
       </div>
       <div>
-        <v-card class="d-flex flex-row justify-center" flat tile>
-         <span class="pa-4 font-weight-bold">
+        <v-card color="transparent" class="d-flex flex-row justify-center" flat tile>
+          <span class="pa-4 font-weight-bold">
             3
             <p class="grey--text caption font-weight-bold">Years of <br>Work</p>
-         </span>
-
-         <span class="pa-4 font-weight-bold">
-           +50
+          </span>
+          <span class="pa-4 font-weight-bold">
+            +50
             <p class="grey--text caption font-weight-bold">Completed <br>Projects</p>
-         </span>
-
-         <span class="pa-4 font-weight-bold">
-           96
-          <p class="grey--text caption font-weight-bold">Satisfied <br>Costumers</p>
+          </span>
+          <span class="pa-4 font-weight-bold">
+            96
+            <p class="grey--text caption font-weight-bold">Satisfied <br>Costumers</p>
           </span>
         </v-card>
       </div>
@@ -43,7 +41,8 @@
         <v-row>
           <v-col cols="6">
             <div align="right">
-              <v-btn depressed dark color="#36aae4" class="font-weight-bold text-capitalize btn-download">
+              <v-btn depressed height="55" rounded large dark color="#36aae4"
+                class="font-weight-bold text-capitalize btn-download">
                 Resume
                 <v-icon right dark>
                   mdi-cloud-download-outline
@@ -51,21 +50,83 @@
               </v-btn>
             </div>
           </v-col>
-            <v-col cols="6">
-              <div align="left" class="">
-                <v-chip label>
-                  <a class="px-1" href="#" target="_blank">
-                    <v-icon color="#323232">mdi-whatsapp</v-icon>
-                  </a>
-                </v-chip>
-                <v-chip label>
-                  <a class="px-1" href="#" target="_blank">
-                    <v-icon color="#323232">mdi-facebook-messenger</v-icon>
-                  </a>
-                </v-chip>
-              </div>
+          <v-col cols="6">
+            <div align="left">
+              <v-chip large>
+                <a class="px-1" href="#" target="_blank">
+                  <v-icon color="#323232">mdi-whatsapp</v-icon>
+                </a>
+              </v-chip>
+              <v-chip large>
+                <a class="px-1" href="#" target="_blank">
+                  <v-icon color="#323232">mdi-facebook-messenger</v-icon>
+                </a>
+              </v-chip>
+            </div>
           </v-col>
         </v-row>
+        <v-row justify="center">
+            <div color="#e8e8e8" class="div-filter">
+              <v-btn @click="showContentFilter('prj')" depressed>Projects</v-btn>
+              <v-btn @click="showContentFilter('skl')" depressed>Skills</v-btn>
+            </div>
+        </v-row>
+      </div>
+      <div class="pa-12 ma-12 mt-0 content-show" >
+        <Transition name="slide-fade">
+          <div v-if="showSkills" class="text-center">
+            <v-chip class="ma-2" color="indigo" text-color="white">
+              <v-avatar left>
+                <v-icon>mdi-account-circle</v-icon>
+              </v-avatar>
+              Mike
+            </v-chip>
+            <v-chip class="ma-2" color="orange" text-color="white">
+              Premium
+              <v-icon right>
+                mdi-star
+              </v-icon>
+            </v-chip>
+            <v-chip class="ma-2" color="primary" text-color="white">
+              1 Year
+              <v-icon right>
+                mdi-cake-variant
+              </v-icon>
+            </v-chip>
+            <v-chip class="ma-2" color="green" text-color="white">
+              <v-avatar left class="green darken-4">
+                1
+              </v-avatar>
+              Years
+            </v-chip>
+            <v-chip class="ma-2" color="teal" text-color="white">
+              <v-avatar left>
+                <v-icon>mdi-checkbox-marked-circle</v-icon>
+              </v-avatar>
+              Confirmed
+            </v-chip>
+            <v-chip class="ma-2" color="teal" text-color="white" close-icon="mdi-delete" >
+              <v-avatar left>
+                <v-icon>mdi-checkbox-marked-circle</v-icon>
+              </v-avatar>
+              Confirmed
+            </v-chip>
+          </div>
+        </Transition>
+        <Transition name="slide-fade">
+          <v-row align-content="center" v-if="showProjects">
+            <v-col v-for="n in 4" :key="n" class="d-flex child-flex" cols="6">
+              <v-img :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+                :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`" aspect-ratio="1" class="grey lighten-2">
+                <template v-slot:placeholder>
+                  <v-row class="fill-height ma-0" align="center" justify="center">
+                    <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                  </v-row>
+                </template>
+              </v-img>
+            </v-col>
+          </v-row>
+        </Transition>
       </div>
     </v-container>
   </div>
@@ -73,11 +134,33 @@
 
 <script>
   export default {
-
+    data() {
+      return {
+        showProjects: true,
+        showSkills: false
+      }
+    },
+    methods: {
+      showContentFilter(value) {
+        if (value == 'prj') {
+          this.showSkills = false;
+          this.showProjects = true;
+        } else if (value == 'skl') {
+          this.showSkills = true;
+          this.showProjects = false;
+        }
+      }
+    }
   }
 
 </script>
 <style lang="scss">
+  .v-application--wrap{
+    //background-image: linear-gradient(to bottom, #ffffff, #f8f8f8, #f0f0f0, #e9e9e9, #e2e2e2);
+    background: radial-gradient(circle, rgba(255,255,255,1) 63%, rgba(221,221,221,1) 100%);
+    
+    
+  }
   .main {
     color: #494949;
 
@@ -85,9 +168,40 @@
       text-decoration: none;
     }
 
+    .media-icon:hover {
+      color: #36aae4 !important;
+    }
+
     .profile-picture {
       border: 4px solid #36aae4;
     }
+
+    .div-filter {
+      margin: 2rem 0 2.5rem;
+      background-color: #e8e8e8;
+      padding: .375rem;
+      border-radius: .75rem;
+      display: flex;
+      justify-content: space-between;
+      column-gap: .5rem;
+
+    }
+
+    //-------INICIO CSS TRANSICTION-----------
+    .slide-fade-enter-active {
+      transition: all 0.2s ease-out;
+    }
+
+    .slide-fade-leave-active {
+      transition: all 0.5s cubic-bezier(1, 0.5, 0.8, 1);
+    }
+
+    .slide-fade-enter-from,
+    .slide-fade-leave-to {
+      transform: translateX(20px);
+      opacity: 0;
+    }
+
   }
 
 </style>
